869e286a948979b65f7bacdd9aa7db4ffbb19840
rajesh6161 <rajeshranjan2712000@gmail.com>
2020-09-04 11:37:06 +0530

    added config

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
 
 const app = express();
 
 // Connect to DB
 mongoose
-  .connect(
-    "mongodb+srv://rajesh:175812@cluster0.ymiio.gcp.mongodb.net/movie?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => {
     console.log(err);
     process.exit(0);
   });
 
-app.get("*", (req, res) => {
-  res.send("Hello World!");
+// Middlewares
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/user/register", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json("Error: " + err.message);
+    console.error(err.message);
+  }
 });
 
 app.listen(5000, () => {
